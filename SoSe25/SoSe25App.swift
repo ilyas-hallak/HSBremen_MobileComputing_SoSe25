2415045f07ce04c958ce7bbaab3e9501e63ca833
Ilyas Hallak <ihallak@neusta.de>
2025-04-23 13:35:40 +0200

    Initial Commit

---
+++ SoSe25/SoSe25App.swift
@@ -0,0 +1,20 @@
+//
+//  SoSe25App.swift
+//  SoSe25
+//
+//  Created by Ilyas Hallak on 23.04.25.
+//
+
+import SwiftUI
+
+@main
+struct SoSe25App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ SoSe25Tests/SoSe25Tests.swift
@@ -0,0 +1,16 @@
+//
+//  SoSe25Tests.swift
+//  SoSe25Tests
+//
+//  Created by Ilyas Hallak on 23.04.25.
+//
+
+import Testing
+
+struct SoSe25Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
